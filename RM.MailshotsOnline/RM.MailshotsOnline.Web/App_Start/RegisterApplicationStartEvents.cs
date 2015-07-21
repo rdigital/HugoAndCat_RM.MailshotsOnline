5dc35b7afbea13861429179a9e99c6b3871a21cc
Mark Radford <ext-mradford@hugoandcat.com>
2015-07-21 18:07:28 +0100

    Attempting to set up Cloud Service properly

---
+++ RM.MailshotsOnline/RM.MailshotsOnline.Web/App_Start/RegisterApplicationStartEvents.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Azure;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using Umbraco.Core;
+
+namespace RM.MailshotsOnline.Web.App_Start
+{
+    public class RegisterApplicationStartEvents : ApplicationEventHandler
+    {
+        protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            base.ApplicationStarting(umbracoApplication, applicationContext);
+            TelemetryConfiguration.Active.InstrumentationKey = CloudConfigurationManager.GetSetting("AppInsightsKey") ?? ConfigurationManager.AppSettings["AppInsightsKey"];
+        }
+    }
+}
