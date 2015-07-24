38b6fd75ffee2bf01abe6c6c19c1dcdc8aaa5d18
Mark Radford <ext-mradford@hugoandcat.com>
2015-07-24 10:56:17 +0100

    Update to project references to stop unit tests throwing exceptions on teardown.

--- RM.MailshotsOnline/RM.MailshotsOnline.Data/Services/PricingService.cs
+++ RM.MailshotsOnline/RM.MailshotsOnline.Data/Services/PricingService.cs
@@ -1,4 +1,5 @@
-﻿using RM.MailshotsOnline.Data.DAL;
+﻿using Microsoft.Azure;
+using RM.MailshotsOnline.Data.DAL;
 using RM.MailshotsOnline.PCL.Models;
 using RM.MailshotsOnline.PCL.Services;
 using System;
@@ -13,11 +14,27 @@ namespace RM.MailshotsOnline.Data.Services
     {
         private StorageContext _context;
 
+        /// <summary>
+        /// Creates a new instance of the PricingService class
+        /// </summary>
         public PricingService()
         {
             _context = new StorageContext();
         }
 
+        /// <summary>
+        /// Creates a new instance of the PricingService class, specifying the connection string name to pass to the Storage Context.
+        /// Added to allow unit tests to specify the connection string properly.
+        /// </summary>
+        /// <param name="connectionStringName">Connection string name</param>
+        public PricingService(string connectionStringName)
+        {
+            _context = new StorageContext(connectionStringName);
+        }
+
+        /// <summary>
+        /// Gets the Postal Options
+        /// </summary>
         public IEnumerable<IPostalOption> GetPostalOptions(int formatId = 0)
         {
             if (formatId > 0)
