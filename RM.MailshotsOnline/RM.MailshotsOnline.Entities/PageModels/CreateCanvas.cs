2c4ebecacd51cda411a6dfc143ff50eba9ba61a8
Mark Radford <ext-mradford@hugoandcat.com>
2015-08-21 10:34:17 +0100

    Added Create Canvas page type and template ahead of integrating James' UI work

---
+++ RM.MailshotsOnline/RM.MailshotsOnline.Entities/PageModels/CreateCanvas.cs
@@ -0,0 +1,14 @@
+﻿using Glass.Mapper.Umb.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RM.MailshotsOnline.Entities.PageModels
+{
+    [UmbracoType(AutoMap = true)]
+    public class CreateCanvas : BasePage
+    {
+    }
+}
