2739f6127812ef9116bd606a1e9b91a00e025d01
wackro <wackro@gmail.com>
2015-10-05 13:05:37 +0100

    DI tweaks

--- RM.MailshotsOnline/RM.MailshotsOnline.WorkerRole/Installers/WorkerRoleInstaller.cs
+++ RM.MailshotsOnline/RM.MailshotsOnline.WorkerRole/Installers/WorkerRoleInstaller.cs
@@ -16,24 +16,9 @@ namespace RM.MailshotsOnline.WorkerRole.Installers
     {
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
-            // Register filtered controllers only (stops Umbraco from breaking)
-            // See: http://stackoverflow.com/questions/21835555/umbraco-mvc-with-castle-windsor
-            // Also register Glass Mapper items and DAL Services
             container.Register(Classes.FromThisAssembly().BasedOn<ThreadedRoleEntryPoint>().LifestyleTransient(),
                 Component.For<ILogger>().ImplementedBy<Logger>().LifestyleTransient(),
                 Component.For<IAuthTokenService>().ImplementedBy<AuthTokenService>().LifestyleTransient());
-        }
-
-        static public string AssemblyDirectory
-        {
-            //Snippet code from: https://gist.github.com/iamkoch/2344638
-            get
-            {
-                var codeBase = Assembly.GetExecutingAssembly().CodeBase;
-                var uri = new UriBuilder(codeBase);
-                var path = Uri.UnescapeDataString(uri.Path);
-                return Path.GetDirectoryName(path);
-            }
         }
     }
 }
