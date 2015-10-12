dada1ea176537dc4d6c38c3e63b3ccac38b71aff
Vicki Keeley <vicki.keeley@gmail.com>
2015-10-12 11:06:29 +0100

    contact/list deletion

--- RM.MailshotsOnline/RM.MailshotsOnline.Web/scripts/src/components/listDetail.js
+++ RM.MailshotsOnline/RM.MailshotsOnline.Web/scripts/src/components/listDetail.js
@@ -20,7 +20,7 @@ define(['jquery', 'knockout', 'view-models/state'],
                 } else {
                     return ko.utils.arrayFilter(this.contacts(), function(contact) {
                         for(var i in contact) {
-                            if (contact[i] && contact[i].toLowerCase().indexOf(self.searchTerm().toLowerCase()) >= 0) {
+                            if (contact[i] && !ko.isObservable(contact[i]) && contact[i].toLowerCase().indexOf(self.searchTerm().toLowerCase()) >= 0) {
                                 return true;
                             }
                         }
@@ -48,10 +48,10 @@ define(['jquery', 'knockout', 'view-models/state'],
         listDetailComponentViewModel.prototype.toggleSelect = function toggleSelect(contact) {
             if (contact.selected() === false) {
                 contact.selected(true);
-                this.selectedContacts.push(contact);
+                this.selectedContacts.push(contact.ContactId);
             } else {
                 contact.selected(false);
-                var pos = this.selectedContacts().map(function(e) { return e.ContactId; }).indexOf(contact.ContactId);
+                var pos = this.selectedContacts().indexOf(contact.ContactId);
                 this.selectedContacts.splice(pos, 1);
             }
         };
@@ -79,6 +79,33 @@ define(['jquery', 'knockout', 'view-models/state'],
 
         listDetailComponentViewModel.prototype.deleteContacts = function deleteContacts() {
             console.log('deleteContacts');
+            var data = {
+                    "DistributionListId": this.listID,
+                    "ContactIds": this.selectedContacts()
+                },
+                self = this;
+
+            $.post('/Umbraco/Api/DistributionList/PostDeleteContactsFromList/', data, function(data) {
+                if (data === null) {
+                    window.location = '/lists';
+                } else {
+                    data.Contacts.forEach(function(contact){
+                        contact.selected = ko.observable(false);
+                    });
+                    self.count(data.List.RecordCount);
+                    self.list(data.List);
+                    self.contacts(data.Contacts);
+                    self.selectedContacts([]);
+                }
+            }).fail(function(error) {
+                stateViewModel.showError(true);
+                if (error.reponseJSON) {
+                    stateViewModel.errorTitle(error.responseJSON.error);
+                } else {
+                    stateViewModel.errorTitle("Oops!");
+                    stateViewModel.errorMessage("Looks like something went wrong, please try again");
+                }
+            });
         };
 
         return {
