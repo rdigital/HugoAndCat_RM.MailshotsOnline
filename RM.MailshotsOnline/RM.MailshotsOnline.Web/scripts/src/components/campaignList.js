94ce7e14959fa9b17ea6e2faad5c70698ebaa4c1
Vicki Keeley <vicki.keeley@gmail.com>
2015-10-15 10:18:52 +0100

    delete and copy campaign functions

--- RM.MailshotsOnline/RM.MailshotsOnline.Web/scripts/src/components/campaignList.js
+++ RM.MailshotsOnline/RM.MailshotsOnline.Web/scripts/src/components/campaignList.js
@@ -1,31 +1,93 @@
-define(['jquery', 'knockout', 'koMapping', 'view-models/state'],
+define(['jquery', 'knockout', 'koMapping', 'view-models/state', 'view-models/notification'],
 
-    function($, ko, koMapping, stateViewModel) {
+    function($, ko, koMapping, stateViewModel, notificationViewModel) {
 
         // ViewModel
         function campaignListComponentViewModel() {
-        	var self = this;
         	this.campaignList = ko.observableArray([]);
         	this.displayedList = ko.observableArray([]);
+        	this.loading = ko.observable(true);
+
+        	this.deleteCampaign = this.deleteCampaign.bind(this);
+        	this.copy = this.copy.bind(this);
+
+        	this.getCampaigns();
+        }
+
+        campaignListComponentViewModel.prototype.getCampaigns = function getCampaigns() {
+        	var self = this;
+        	this.loading(true);
+
+    		$.ajax({
+    		    url: '/Umbraco/Api/Campaign/GetAll',
+    		    type: 'GET',
+    		    success: function (data) {
+    		        self.campaignList(data);
+    		        self.loading(false);
+    		    },
+    		    error: function(error) {
+    	            stateViewModel.showError(true);
+    	            if (error) {
+    	                stateViewModel.errorTitle("Oops!");
+    	                stateViewModel.errorMessage(error.responseJSON.error);
+    	            } else {
+    	                stateViewModel.errorTitle("Oops!");
+    	                stateViewModel.errorMessage("Looks like something went wrong, please try again");
+    	            }
+    	            self.loading(false);
+    	        }
+    		});
+        };
+
+        campaignListComponentViewModel.prototype.deleteCampaign = function deleteCampaign(data) {
+        	var self = this;
+
+    	    $.ajax({
+    	        url: '/Umbraco/Api/Campaign/Delete/' + data.CampaignId,
+    	        type: 'DELETE',
+    	        success: function () {
+    	            self.getCampaigns();
+    	            notificationViewModel.hideWithMessage("Campaign deleted", 'message');
+    	        },
+    	        error: function(error) {
+                   	stateViewModel.showError(true);
+                   	if (error) {
+                       	stateViewModel.errorTitle("Oops!");
+                       	stateViewModel.errorMessage(error.responseJSON.error);
+                   	} else {
+                       	stateViewModel.errorTitle("Oops!");
+                       	stateViewModel.errorMessage("Looks like something went wrong, please try again");
+                   	}
+               	}
+            });
+        };
+
+        campaignListComponentViewModel.prototype.copy = function copy(data) {
+        	var self = this;
 
         	$.ajax({
-        	    url: '/Umbraco/Api/Campaign/GetAll',
+        	    url: '/Umbraco/Api/Campaign/GetCopy/' + data.CampaignId,
         	    type: 'GET',
-        	    success: function (data) {
-        	        self.campaignList(data);
+        	    success: function () {
+        	        self.getCampaigns();
+        	        notificationViewModel.hideWithMessage("Campign copied", 'addContact');
         	    },
         	    error: function(error) {
-                    stateViewModel.showError(true);
-                    if (error) {
-                        stateViewModel.errorTitle("Oops!");
-                        stateViewModel.errorMessage(error.responseJSON.error);
-                    } else {
-                        stateViewModel.errorTitle("Oops!");
-                        stateViewModel.errorMessage("Looks like something went wrong, please try again");
-                    }
-                }
+                   	stateViewModel.showError(true);
+                   	if (error) {
+                       	stateViewModel.errorTitle("Oops!");
+                       	stateViewModel.errorMessage(error.responseJSON.error);
+                   	} else {
+                       	stateViewModel.errorTitle("Oops!");
+                       	stateViewModel.errorMessage("Looks like something went wrong, please try again");
+                   	}
+               	}
         	});
-        }
+        };
+
+        campaignListComponentViewModel.prototype.createNew = function createNew() {
+        	console.log('createNew');
+        };
 
         return {
             viewModel: campaignListComponentViewModel,
