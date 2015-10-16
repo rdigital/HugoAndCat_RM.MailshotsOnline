3e2445b934bde1b590f840b3910c868aecda3d44
Tim Reed <tim@alloftheabove.co.uk>
2015-10-16 09:34:15 +0100

    draft proceed to stages

--- RM.MailshotsOnline/RM.MailshotsOnline.Web/scripts/src/view-models/register.js
+++ RM.MailshotsOnline/RM.MailshotsOnline.Web/scripts/src/view-models/register.js
@@ -1,132 +1,149 @@
 define([
         'knockout',
+        'jquery',
         'koValidation',
         'select2'
     ],
 
-    function(ko, koValidation) {
+    function(ko, $, koValidation) {
 
         function registerViewModel() {
+
+            // create the stages (each part of the form)
             this.stage1 = {};
             this.stage2 = {};
-            this.stage3 = {};
 
 
-            this.stage1.Email = ko.observable().extend({ 
-                required: {
-                    email: true
+            this.init = function(argument) {
+                // hide the sections
+                $('.register-stage').hide();
+                this.stage1Init();
+                this.stage1Errors = koValidation.group(this.stage1, {deep: true});
+                this.stage1Errors.showAllMessages(false);
+            }
+
+
+            this.stage1Init = function() {
+                $('.register-stage').eq(0).show();
+                
+                this.stage1.Email = ko.observable().extend({ 
+                    required: {
+                        message: 'Please enter your email address'
+                    },
+                    pattern: {
+                        message: 'Please enter a valid email address',
+                        params: "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+                    }
+                });
+                this.stage1.ConfirmEmail = ko.observable().extend({ 
+                    required: {
+                        message: 'Please re-enter a valid email address'
+                    }
+                });
+                this.stage1.FirstName = ko.observable().extend({ 
+                    required: {
+                        message: 'Please enter your first name'
+                    }
+                });
+                this.stage1.LastName = ko.observable().extend({ 
+                    required: {
+                        message: 'Please enter your last name'
+                    }
+                });
+                this.stage1.Title = ko.observable().extend({ 
+                    required: {
+                        message: 'Please select a Title'
+                    }
+                });
+                this.stage1.Password = ko.observable().extend({ 
+                    required: {
+                        message: 'Please enter an alphanumeric password of eight characters or more'
+                    }
+                });
+                this.stage1.ConfirmPassword = ko.observable().extend({ 
+                    required: {
+                        message: 'Please re-type your password.'
+                    }
+                });
+                this.stage1.royalMailMailAgreement = ko.observable();
+                this.stage1.thirdPartyMailAgreement = ko.observable();
+                this.stage1.AgreeToTermsAndConditions = ko.observable().extend({
+                    // required: {
+                    //     message: 'Please accept T&C agreement'
+                    // }
+                });
+            }
+
+            this.stage2Init = function() {
+                this.stage2.Postcode = ko.observable().extend({ 
+                    required: {
+                        message: 'Please'
+                    }
+                });
+                this.stage2.Organisation = ko.observable().extend({ 
+                    required: {
+                        message: 'Please'
+                    }
+                });
+                this.stage2.Address1 = ko.observable().extend({ 
+                    required: {
+                        message: 'Please'
+                    }
+                });
+                this.stage2.Address2 = ko.observable().extend({ 
+                    required: {
+                        message: 'Please'
+                    }
+                });
+                this.stage2.Town = ko.observable().extend({ 
+                    required: {
+                        message: 'Please'
+                    }
+                });
+                this.stage2.City = ko.observable().extend({ 
+                    required: {
+                        message: 'Please'
+                    }
+                });
+                this.stage2.Country = ko.observable().extend({ 
+                    required: {
+                        message: 'Please'
+                    }
+                });
+                this.stage2.PhoneWork = ko.observable().extend({ 
+                    required: {
+                        message: 'Please'
+                    }
+                });
+                this.stage2.PhoneMobile = ko.observable().extend({ 
+                    required: {
+                        message: 'Please'
+                    }
+                });
+                this.stage2.Email = ko.observable().extend({ 
+                    required: {
+                        message: 'Please'
+                    }
+                });
+
+                this.stage2Errors = koValidation.group(this.stage2, {deep: true});
+                this.stage2Errors.showAllMessages(false);
+            }
+
+            this.proceedToStage2 = function() {
+                if (this.stage1Errors().length === 0) {
+                    $('.register-stage').eq(0).hide();
+                    $('.register-stage').eq(1).show();
+
+                    this.stage2Init();
                 }
-            });
-            this.stage1.ConfirmEmail = ko.observable().extend({ 
-                required: {
-                    message: 'Please re-enter a valid email address'
+                else {
+                    console.log(this.stage1Errors());
+                    this.stage1Errors.showAllMessages();
                 }
-            });
-            this.stage1.FirstName = ko.observable().extend({ 
-                required: {
-                    message: 'Please enter your first name'
-                }
-            });
-            this.stage1.LastName = ko.observable().extend({ 
-                required: {
-                    message: 'Please enter your last name'
-                }
-            });
-            this.stage1.Title = ko.observable().extend({ 
-                required: {
-                    message: 'Please select a Title'
-                }
-            });
-            this.stage1.Password = ko.observable().extend({ 
-                required: {
-                    message: 'Please enter an alphanumeric password of eight characters or more'
-                }
-            });
-            this.stage1.ConfirmPassword = ko.observable().extend({ 
-                required: {
-                    message: 'Please re-type your password.'
-                }
-            });
-            this.stage1.royalMailMailAgreement = ko.observable();
-            this.stage1.thirdPartyMailAgreement = ko.observable();
+            };
 
-            this.stage1.AgreeToTermsAndConditions = ko.observable().extend({ 
-                required: {
-                    message: 'Please accept T&amp;C agreement'
-                }
-            });
-
-
-
-
-            this.stage2.Postcode = ko.observable().extend({ 
-                required: {
-                    message: 'Please'
-                }
-            });
-
-
-            this.stage2.Organisation = ko.observable().extend({ 
-                required: {
-                    message: 'Please'
-                }
-            });
-
-
-            this.stage2.Address1 = ko.observable().extend({ 
-                required: {
-                    message: 'Please'
-                }
-            });
-
-            this.stage2.Address2 = ko.observable().extend({ 
-                required: {
-                    message: 'Please'
-                }
-            });
-
-            this.stage2.Town = ko.observable().extend({ 
-                required: {
-                    message: 'Please'
-                }
-            });
-
-            this.stage2.City = ko.observable().extend({ 
-                required: {
-                    message: 'Please'
-                }
-            });
-
-            this.stage2.Country = ko.observable().extend({ 
-                required: {
-                    message: 'Please'
-                }
-            });
-
-            this.stage2.PhoneWork = ko.observable().extend({ 
-                required: {
-                    message: 'Please'
-                }
-            });
-
-            this.stage2.PhoneMobile = ko.observable().extend({ 
-                required: {
-                    message: 'Please'
-                }
-            });
-
-            this.stage2.Email = ko.observable().extend({ 
-                required: {
-                    message: 'Please'
-                }
-            });
-
-            this.stage1Errors = koValidation.group(this.stage1, {deep: true});
-            this.stage2Errors = koValidation.group(this.stage2, {deep: true});
-            this.stage3Errors = koValidation.group(this.stage2, {deep: true});
-
-            this.proceedToNextStage = function() {
-                console.log('proceedToNextStage');
+            this.submit = function() {
                 if (this.stage1Errors().length === 0) {
                     alert('Thank you.');
                 }
@@ -134,13 +151,6 @@ define([
                     alert('Please check your submission.');
                     this.stage1Errors.showAllMessages();
                 }
-            };
-
-            this.stage1Errors.showAllMessages(false);
-            this.stage2Errors.showAllMessages(false);
-            this.stage3Errors.showAllMessages(false);
-
-            this.submit = function() {
                 if (this.stage2Errors().length === 0) {
                     alert('Thank you.');
                 }
@@ -148,17 +158,11 @@ define([
                     alert('Please check your submission.');
                     this.stage2Errors.showAllMessages();
                 }
-                if (this.stage3Errors().length === 0) {
-                    alert('Thank you.');
-                }
-                else {
-                    alert('Please check your submission.');
-                    this.stage3Errors.showAllMessages();
-                }
             }
 
-        }
+            this.init();
 
+        }
 
 
         return new registerViewModel();
